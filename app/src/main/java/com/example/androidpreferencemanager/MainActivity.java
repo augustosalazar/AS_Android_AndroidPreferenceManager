e1e39f1faee6abf6d9ab3084cc8828e9938e5142
Augusto Salazar <augustosalazar@uninorte.edu.co>
2015-02-03 18:19:00 -0500

    Algunas mejoras visuales

--- app/src/main/java/com/example/androidpreferencemanager/MainActivity.java
+++ app/src/main/java/com/example/androidpreferencemanager/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         
         mTextView = (TextView) findViewById(R.id.textView1);
