9a3dd2592270c25710248a1a7499311fc697d8fb
Augusto Salazar <augustosalazar@uninorte.edu.co>
2015-02-09 16:36:14 -0500

    Add activity for configuration.

--- app/src/main/java/com/example/androidpreferencemanager/MainActivity.java
+++ app/src/main/java/com/example/androidpreferencemanager/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.androidpreferencemanager;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 	protected SharedPreferences mSharedPreferences;
 	protected String skey = "PREF_CHECKBOX_STRING";
 	protected TextView mTextView;
@@ -54,4 +57,29 @@ public class MainActivity extends Activity {
 		});
         
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this,SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
